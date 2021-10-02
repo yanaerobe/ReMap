a2cef4244b68b869d5d53659e8d910db4b9a333b
YJY1029 <839665724@qq.com>
2021-10-02 23:01:09 +0800

    feat: calculated all ratios

--- Checked/mappingback.m
+++ Checked/mappingback.m
@@ -11,22 +11,23 @@ function mappingback
 Gr = [2^0+2^-2, 2^0, 2^0-2^-3, 2^0-2^-2]; %1.25, 1, 0.875, 0.75
 m2 = [
 	0.25, 0.34375, 0.53125, 0.5625; 
-    0, 0.25, 0.34375, 0.5; 
-	0, 0.3125, 0.34375, 0.53125; 
-    0, 0.3125, 0.34375, 0.5
+    0.25, 0.25, 0.34375, 0.5; 
+	0.3125, 0.3125, 0.34375, 0.53125; 
+    0.3125, 0.3125, 0.34375, 0.5; %这个0.5有问题，为什么？
 	]; 
 
-for N = (1 : 1 : 1) 
+for N = (1 : 1 : 4) 
 	m1 = (m2(N,:)+1)/Gr(N) -1; 
     fm1 = log2(1+m1);
-    for M = (1 :1 :4) 
-        gm1(M) = castrARM4(m1(M)); 
+    for M = (1 : 1 : 4) 
+        gm1(M) = castrARM4(m1(M)); %可能不能用m1，这算出来好像没啥用啊，还是说gm1不对？
     end
-    x_ratio = m1/m2; 
-    y_ratio = fm1/gm1; 
+    x_ratio = m1./m2(N,:); 
+    y_ratio = fm1./gm1; 
+    disp(N); 
     disp(['m1: ', num2str(m1)]); 
     disp(['fm1: ', num2str(fm1)]); 
 	disp(['gm1: ', num2str(gm1)]); 
-    disp(['ratios of m1/m2: ', num2str(x_ratio)]); %快了，这里是矩阵相除有问题
-    disp(['ratios of fm1/gm2: ', num2str(y_ratio)]); 
+    disp(['m1/m2: ', num2str(x_ratio)]); 
+    disp(['fm1/gm2: ', num2str(y_ratio)]); 
 end
