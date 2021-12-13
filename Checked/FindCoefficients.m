2959cece1973cedb7ca3c455a563c4e293ebbeee
YJY1029 <839665724@qq.com>
2021-12-13 22:01:34 +0800

    style: separate make file added for cleaner view

--- Checked/FindCoefficients.m
+++ Checked/FindCoefficients.m
@@ -3,54 +3,38 @@
 
 %for each rgnment, gm1 is the first point and fm1 the second. 
 
-format long; 
-
-Gr = [
-    2^0+2^-2, 2^0+2^-2, 2^0+2^-2, 2^0+2^-2, 2^0+2^-2; 
-    2^0, 2^0, 2^0, 2^0, 2^0; 
-    2^0-2^-3, 2^0-2^-3, 2^0-2^-3, 2^0-2^-3, 2^0-2^-3; 
-    2^0-2^-2, 2^0-2^-2, 2^0-2^-2, 2^0-2^-2, 2^0-2^-2; 
-    ]; %1.25, 1, 0.875, 0.75
-
-m2 = [
-	0.25, 0.34375, 0.375, 0.53125, 0.5625; 
-    0.25, 0.34375, 0.375, 0.5, 0.5; 
-	0.3125, 0.34375, 0.375, 0.53125, 0.53125; 
-    0.3125, 0.34375, 0.375, 0.5, 0.5; 
-	]; 
-
-	m1 = (m2+1)./Gr-1; 
+make; 
     
-    file = 'FindCoefficients.txt'; 
-    delete(file); 
-    fid = fopen(file, 'a'); 
+file = 'FindCoefficients.txt'; 
+delete(file); 
+fid = fopen(file, 'a'); 
     
-    for rgn = (1 : 4) %region 1
-        if rgn == 1 
-            for seg = (1 : 4) 
-                poly = Fitting(rgn, seg); 
-                fprintf(fid, '%f ', poly); 
-                fprintf(fid, '\n'); 
-                xi = m1(rgn, seg) : 0.00001 : m1(rgn, seg+1)-2^-16; 
-                yi = polyval(poly, xi); 
-                plot(xi, yi); 
-                hold on; 
-            end 
-        else 
-            for seg = (1 : 3) 
-              poly = Fitting(rgn, seg); 
-                fprintf(fid, '%f ', poly); 
-                fprintf(fid, '\n'); 
-                xi = m1(rgn, seg) : 0.00001 : m1(rgn, seg+1)-2^-16; 
-                yi = polyval(poly, xi); 
-                plot(xi, yi); 
-                hold on; 
-            end
+for rgn = (1 : 4) %region 1
+	if rgn == 1 
+        for seg = (1 : 4) 
+            poly = Fitting(rgn, seg); 
+            fprintf(fid, '%f ', poly); 
+            fprintf(fid, '\n'); 
+            %xi = m1(rgn, seg) : 0.00001 : m1(rgn, seg+1)-2^-16; 
+            %yi = polyval(poly, xi); 
+            %plot(xi, yi); 
+            %hold on; 
         end 
-    end 
-    
-    fclose('all'); 
-    
-    x = 0 : 0.001 : 1; 
-    y = log2(1+x); 
-    plot(x, y); 
+    else 
+        for seg = (1 : 3) 
+          poly = Fitting(rgn, seg); 
+            fprintf(fid, '%f ', poly); 
+            fprintf(fid, '\n'); 
+            %xi = m1(rgn, seg) : 0.00001 : m1(rgn, seg+1)-2^-16; 
+            %yi = polyval(poly, xi); 
+            %plot(xi, yi); 
+            %hold on; 
+        end
+	end 
+end 
+
+fclose('all'); 
+
+%x = 0 : 0.001 : 1; 
+%y = log2(1+x); 
+%plot(x, y); 
--- Checked/FindPoints.m
+++ Checked/FindPoints.m
@@ -1,39 +1,24 @@
-format long; 
+make; 
+m1 = (m2+1)./Gr-1; 
+gm1 = reshape(arrayfun(@castrARM4, m1(:)), 4, 5); 
 
-Gr = [
-    2^0+2^-2, 2^0+2^-2, 2^0+2^-2, 2^0+2^-2, 2^0+2^-2; 
-    2^0, 2^0, 2^0, 2^0, 2^0; 
-    2^0-2^-3, 2^0-2^-3, 2^0-2^-3, 2^0-2^-3, 2^0-2^-3; 
-    2^0-2^-2, 2^0-2^-2, 2^0-2^-2, 2^0-2^-2, 2^0-2^-2; 
-    ]; %1.25, 1, 0.875, 0.75
-
-m2 = [
-	0.25, 0.34375, 0.375, 0.53125, 0.5625; 
-    0.25, 0.34375, 0.375, 0.5, 0.5; 
-	0.3125, 0.34375, 0.375, 0.53125, 0.53125; 
-    0.3125, 0.34375, 0.375, 0.5, 0.5; %forcibly included m1 = 1 into the orginal algo
-	]; 
+file = 'FindPoints.txt'; 
+delete(file); 
+fid = fopen(file, 'a'); 
     
-	m1 = (m2+1)./Gr-1; 
-    gm1 = reshape(arrayfun(@castrARM4, m1(:)), 4, 5); 
-    
-    file = 'FindPoints.txt'; 
-    delete(file); 
-    fid = fopen(file, 'a'); 
-    
-    for I = (1 : 4) 
-        %disp(I); 
-        %disp(['m1: ', num2str(m1(I,:))]); 
-        %disp(['m2: ', num2str(m2(I,:))]); 
-        %disp(['gm1: ', num2str(gm1(I,:))]); 
-        %disp(['m1/m2: ', num2str(x_ratio(I,:))]); 
-        %disp(['fm1/gm2: ', num2str(y_ratio(I,:))]); 
-        fprintf (fid, 'Segment %d:\nm1: ', I); 
-        fprintf (fid, '%f ', m1(I,:)); 
-        fprintf (fid, '\nm2: '); 
-        fprintf (fid, '%f ', m2(I,:)); 
-        fprintf (fid, '\ngm1: '); 
-        fprintf (fid, '%f ', gm1(I,:)); 
-        fprintf (fid, '\n'); 
-    end 
-    fclose('all'); 
+for I = (1 : 4) 
+	%disp(I); 
+	%disp(['m1: ', num2str(m1(I,:))]); 
+	%disp(['m2: ', num2str(m2(I,:))]); 
+	%disp(['gm1: ', num2str(gm1(I,:))]); 
+	%disp(['m1/m2: ', num2str(x_ratio(I,:))]); 
+	%disp(['fm1/gm2: ', num2str(y_ratio(I,:))]); 
+	fprintf (fid, 'Segment %d:\nm1: ', I); 
+	fprintf (fid, '%f ', m1(I,:)); 
+	fprintf (fid, '\nm2: '); 
+	fprintf (fid, '%f ', m2(I,:)); 
+	fprintf (fid, '\ngm1: '); 
+	fprintf (fid, '%f ', gm1(I,:)); 
+	fprintf (fid, '\n'); 
+end 
+fclose('all'); 
