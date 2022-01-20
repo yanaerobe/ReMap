14c581e096368a296b31a3364571c83705130a48
YJY1029 <839665724@qq.com>
2022-01-20 14:46:47 +0800

    feat: restarting fitting method

--- Checked/FindCoefficients.m
+++ Checked/FindCoefficients.m
@@ -1,11 +1,28 @@
+% another bug described in container/remap.md
+% 1. modify Fitting.md into a function; 2. adopt Fitting(rgn) in FindCoefficients; 3. continue 16 bit simulation process and assess its performance. 
+
+%for each segment, gm1 is the first point and fm1 the second. 
+
+make; 
+    
 file = 'FindCoefficients.txt'; 
 delete(file); 
 fid = fopen(file, 'a'); 
     
-for i = 1 : 41 
-    poly = Fitting(i); 
-    fprintf(fid, '%.7f ', poly); 
-    fprintf(fid, '\n'); 
+for rgn = (1 : 4) %region 1
+	if rgn == 1 
+        for seg = (1 : 5) 
+            poly = Fitting(rgn, seg); 
+            fprintf(fid, '%.7f ', poly); 
+            fprintf(fid, '\n'); 
+        end 
+    else 
+        for seg = (1 : 4) 
+          poly = Fitting(rgn, seg); 
+            fprintf(fid, '%.7f ', poly); 
+            fprintf(fid, '\n'); 
+        end
+	end 
 end 
 
 fclose('all'); 
