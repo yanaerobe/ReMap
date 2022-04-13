98bbd1961c693f93989132b74b4075af9eea0c1d
YJY1029 <839665724@qq.com>
2022-04-13 23:05:15 +0800

    feat: add scripts

---
+++ scripts/rm_make.tcl
@@ -0,0 +1,33 @@
+# For range mapping synthesis
+# read files
+read_verilog ./design_files/rm_lod32_rom.v
+read_verilog ./design_files/rm_logshifter32.v
+read_verilog ./design_files/rm_mapping.v
+read_verilog ./design_files/rm_errorcorrection.v
+read_verilog ./design_files/rm_top.v 
+
+# constraints 
+current_design m_rangemapping 
+source "${script_path}/rm_constr.con" 
+
+# design environment 
+set_load $MAX_OUTPUT_LOAD [all_outputs] 
+set_driving_cell -lib_cell $DRIVE_CELL [all_inputs]
+
+# design constraints 
+set_input_delay  $INPUT_DELAY -clock $CLK_PORT [all_inputs]
+set_output_delay $OUTPUT_DELAY -clock $CLK_PORT [all_outputs]
+set_max_area 0
+
+# ungroup DesignWare parts 
+set designware_cells [get_cells -filter "@is_oper==true" *]
+if {[sizeof_collection $designware_cells] > 0} {
+   set_ungroup $designware_cells true
+}
+
+set_fix_hold [all_clocks]
+compile_ultra
+write -format verilog -output ${netlist_path}/syn_$design.v -hier
+write -format ddc -hierarchy -output "${ddc_path}/${design}.ddc"
+write_sdf ${netlist_path}/syn_$design.sdf
+# source "${script_path}/report.tcl"
