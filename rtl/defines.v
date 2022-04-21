32801e258bcf92ebf18ccf27572ec0a6270348b0
YJY1029 <839665724@qq.com>
2022-04-21 23:56:55 +0800

    feat: verilog completed

--- rtl/defines.v
+++ rtl/defines.v
@@ -3,11 +3,5 @@
 `define M1_LENGTH  28
 `define M2_LENGTH  27 //NOTE THAT 28-bit m processing, 27-bit m output
 
-`define NODE_NUM   7
-`define PIECE_NUM  `NODE_NUM - 1
-
-`define SEG_NUM    4
-`define SEG1_NUM   2 
-`define SEG2_NUM   2 
-`define SEG3_NUM   1 
-`define SEG4_NUM   1
+`define PIECE_NUM  13
+`define NODE_NUM   `PIECE_NUM + 1
